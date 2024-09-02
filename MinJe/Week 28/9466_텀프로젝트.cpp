75fc18af08755e48e8d4b793d159874065406d24
kevinmj12 <kevinmj12@gmail.com>
2024-09-02 23:35:18 +0900

    Minje Week 28

--- MinJe/Week 28/9466_텀프로젝트.cpp
+++ MinJe/Week 28/9466_텀프로젝트.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <deque>
+#include <stack>
 using namespace std;
 
 #define MAX 100001
@@ -7,7 +7,7 @@ int t, n;
 int s[MAX];
 bool visited[MAX];
 int answer = 0;
-deque<int> students;
+stack<int> students;
 
 void reset(){
     answer = 0;
@@ -18,7 +18,7 @@ void reset(){
 
 void solve(int student){
     visited[student] = true;
-    students.push_back(student);
+    students.push(student);
     int next = s[student];
 
     // 선택한 학생이 본인과 같다면 리턴
@@ -29,15 +29,15 @@ void solve(int student){
     // 선택한 학생이 이미 선택된 학생이라면
     if (visited[next]){
         int studentsSize = students.size();
-        // 이미 선택된 학생을 찾기 위해 pop_back을 반복
-        while (!students.empty() && students.back() != next){
-            students.pop_back();
+        // 이미 선택된 학생을 찾기 위해 pop을 반복
+        while (!students.empty() && students.top() != next){
+            students.pop();
         }
-        // 만약 deque에 원소가 존재한다면 사이클이 존재함
+        // 만약 스택에 원소가 존재한다면 사이클이 존재함
         if (!students.empty()){
             answer += (studentsSize-students.size()+1);
         }
-        // 만약 deque이 비어있다면 사이클이 없는 것임
+        // 만약 스택이 비어있다면 사이클이 없는 것임
         return;
     }
     else{
@@ -62,7 +62,9 @@ int main(){
         
         for (int i = 1; i <= n; i++){
             if (!visited[i]){
-                students.clear();
+                while (!students.empty()){
+                    students.pop();
+                }
                 solve(i);
             }
         }
